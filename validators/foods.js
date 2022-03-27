914f05ae6abbe5784addf3ee490506308e1ac6ac
MJavad Hasanzade <hjavad522@gmail.com>
2022-03-27 21:12:07 +0430

    feat: create foods validator

---
+++ validators/foods.js
@@ -0,0 +1,43 @@
+const Joi = require("joi");
+
+const foodValidator = (data) => {
+  let schema;
+
+  if (data.method === "POST") {
+    schema = Joi.object({
+      name: Joi.string().required().messages({
+        "any.required": "product_name_null",
+      }),
+      priceByRestaurant: Joi.number().required().min(0).messages({
+        "number.min": "product_price_size",
+      }),
+      priceByIngredient: Joi.number().required().min(0).messages({
+        "number.min": "product_price_size",
+      }),
+      ingredients: Joi.array().items(Joi.number().integer()).required().unique((a, b) => a === b),
+      benefit: Joi.number().required().min(0).messages({
+        "number.min": "product_price_size",
+      }),
+    });
+  } else {
+    schema = Joi.object({
+      name: Joi.string().messages({
+        "any.required": "product_name_null",
+      }),
+      priceByRestaurant: Joi.number().min(0).messages({
+        "number.min": "product_price_size",
+      }),
+      priceByIngredient: Joi.number().min(0).messages({
+        "number.min": "product_price_size",
+      }),
+      ingredients: Joi.array().items(Joi.number().integer()).unique((a, b) => a === b),
+      benefit: Joi.number().min(0).messages({
+        "number.min": "product_price_size",
+      }),
+    });
+  }
+
+  return schema.validate(data.body);
+};
+
+module.exports = foodValidator;
