a9139ef9ae9a1c183919163e3b6e7bfd4db4a5fa
MJavad Hasanzade <hjavad522@gmail.com>
2022-04-29 12:23:22 +0430

    feat: change DB to mysql

--- database.js
+++ database.js
@@ -1,8 +1,17 @@
 const { Sequelize } = require("sequelize");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const sequelize = new Sequelize("food-pricer", "user", "pass", {
-  dialect: "sqlite",
-  host: "./dev.sqlite",
-});
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.USER,
+  process.env.PASS,
+  {
+    dialect: "mysql",
+    host: process.env.HOST,
+    port: process.env.DBPORT,
+  }
+);
 
 module.exports = sequelize;
