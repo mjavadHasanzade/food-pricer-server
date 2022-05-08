372a08c72f3686773e939058fa18a9e484ed8030
MJavad Hasanzade <hjavad522@gmail.com>
2022-05-08 21:41:38 +0430

    feat: add cors package to handle cors errors

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var bodyParser = require("body-parser");
 const Foods = require("./Models/foods");
 const Users = require("./Models/users");
 const Auth = require("./middlewares/Auth");
+const cors = require("cors");
 
 //?Rotes
 const ingredientsRoutes = require("./Routes/ingredients");
 const foodsRoutes = require("./Routes/foods");
 const menusRoutes = require("./Routes/menus");
 const userRoutes = require("./Routes/user");
+const Menus = require("./Models/menus");
 
 require("dotenv").config();
 
@@ -25,13 +27,22 @@ sequelize.sync({ force: true }).then(async () => {
     email: "hjavad522@gmail.com",
   });
 
+  await Users.create({
+    name: "mjavad",
+    family: "hsnzde",
+    role: "user",
+    password: "12345689",
+    isActive: true,
+    email: "hjavad2522@gmail.com",
+  });
+
   for (let i = 1; i <= 15; i++) {
     const ingredient = {
       name: `ingredient ${i}`,
       quantity: Math.floor(Math.random() * 100),
       price: Math.floor(Math.random() * 1000000),
       isComplete: Math.random() > 0.5 ? true : false,
-      UserId: 1,
+      UserId: Math.random() > 0.5 ? 1 : 2,
     };
     await Ingredients.create(ingredient);
     const food = {
@@ -39,9 +50,17 @@ sequelize.sync({ force: true }).then(async () => {
       priceByIngredient: Math.floor(Math.random() * 10000),
       priceByRestaurant: Math.floor(Math.random() * 1000000),
       benefit: Math.floor(Math.random() * 50),
-      UserId: 1,
+      UserId: Math.random() > 0.5 ? 1 : 2,
     };
     await Foods.create(food);
+  }
+  for (let j = 1; j <= 3; j++) {
+    const menu = {
+      name: `Menu ${j}`,
+      isActive: Math.random() > 0.5 ? true : false,
+      UserId: Math.random() > 0.5 ? 1 : 2,
+    };
+    await Menus.create(menu);
   }
   console.log("db synced");
 });
@@ -63,6 +82,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/ingredients", Auth, ingredientsRoutes);
 app.use("/foods", Auth, foodsRoutes);
